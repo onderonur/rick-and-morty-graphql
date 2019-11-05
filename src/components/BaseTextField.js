f58ed06237a6a41a1f7a6fcff073c2709e64b007
onderonur <onuronder.onr@gmail.com>
2019-11-05 23:43:50 +0300

    dependency updates

--- src/components/BaseTextField.js
+++ src/components/BaseTextField.js
@@ -1,32 +1,15 @@
 import React from "react";
 import { TextField } from "@material-ui/core";
-import { Field } from "formik";
-
-function MuiTextField({
-  field, // {name, value, onChange, onBlur}
-  form, // {touched, errors, values, setXXXX, handleXXXX, dirty, isValid, submitCount,status, etc.}
-  ...rest
-}) {
-  const { name } = field;
-  const { touched, errors } = form;
-
-  const error = errors[name];
-  const isTouched = touched[name];
-
-  const hasError = error && isTouched;
-
-  return (
-    <TextField
-      {...field}
-      {...rest}
-      error={hasError}
-      helperText={hasError ? error : ""}
-    />
-  );
-}
+import { useField } from "formik";
 
 function BaseTextField(props) {
-  return <Field {...props} component={MuiTextField} />;
+  const [field, meta] = useField(props);
+  const { error, touched } = meta;
+  const hasError = error && touched;
+
+  return (
+    <TextField {...props} {...field} error={hasError} helperText={error} />
+  );
 }
 
 export default BaseTextField;

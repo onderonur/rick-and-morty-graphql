51c317d76a205cb1f8e0000f8bb18b55cf1286b1
onderonur <onuronder.onr@gmail.com>
2020-04-27 15:47:28 +0300

    github link

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -8,6 +8,7 @@ import {
   makeStyles,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import GitHubIcon from "@material-ui/icons/GitHub";
 import NextLink from "@/shared/components/NextLink";
 import { useToggleDrawerMutation } from "@/generated/graphql";
 
@@ -42,6 +43,18 @@ const AppHeader = React.forwardRef((props, ref) => {
             </Typography>
           </NextLink>
           <Box flexGrow={1} />
+          <IconButton
+            href="https://github.com/onderonur/rick-and-morty-graphql"
+            target="__blank"
+            // https://developers.google.com/web/tools/lighthouse/audits/noopener
+            // rel="noopener" prevents the new page from being able to access the window.opener
+            // property and ensures it runs in a separate process.
+            // rel="noreferrer" attribute has the same effect, but also prevents the Referer header
+            // from being sent to the new page.
+            rel="noopener"
+          >
+            <GitHubIcon />
+          </IconButton>
           <IconButton onClick={handleClick}>
             <MenuIcon />
           </IconButton>

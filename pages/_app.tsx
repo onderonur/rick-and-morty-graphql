28193e4efb0b845c4f3bf0b971519e1147fa846a
onderonur <onuronder.onr@gmail.com>
2020-05-23 18:14:05 +0300

    minor refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,12 +34,14 @@ const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const getDefaultSeoConfig = (pathname: string): DefaultSeoProps => {
   const url = `${NEXT_PUBLIC_BASE_URL}${pathname}`;
+  const description = `${APP_TITLE} is a client application for Rick and Morty GraphQL API. It's created with Next.js, Apollo-Client and TypeScript.`;
   return {
     titleTemplate: `%s | ${APP_TITLE}`,
-    description: `${APP_TITLE} is a client application for Rick and Morty GraphQL API. It's created with Next.js, Apollo-Client and TypeScript.`,
+    description,
     canonical: url,
     openGraph: {
       title: "Rick and Morty GraphQL Application",
+      description,
       type: "website",
       locale: "en_IE",
       url,

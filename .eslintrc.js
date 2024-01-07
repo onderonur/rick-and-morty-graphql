f65d9726d3d7299f2775585a3028987938e36ee8
onderonur <onuronder.onr@gmail.com>
2024-01-07 16:01:04 +0300

    package updates & eslint configs update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
     // https://nextjs.org/docs/pages/building-your-application/configuring/eslint#additional-configurations
@@ -8,10 +9,11 @@ module.exports = {
     'prettier',
     'next/core-web-vitals',
   ],
+  plugins: ['only-warn'],
   reportUnusedDisableDirectives: true,
   // Files starting with . are ignored by default.
   // This was causing a warning for lint-staged
   // and since we have --max-warnings 0, the check was failing.
   // So, we removed these files by using "!" from ignoredPatterns.
-  ignorePatterns: ['!.*.{js,ts}', 'src/gql'],
+  ignorePatterns: ['!.*.{js,ts}'],
 };

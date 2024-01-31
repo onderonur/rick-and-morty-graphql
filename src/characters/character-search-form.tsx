28e978ebaac9e15fed40f2e39afbb2e645481634
onderonur <onuronder.onr@gmail.com>
2024-01-31 12:35:25 +0300

    using <search> element

--- src/characters/character-search-form.tsx
+++ src/characters/character-search-form.tsx
@@ -11,28 +11,31 @@ export function CharacterSearchForm() {
   const keyword = searchParams.get('keyword') ?? '';
 
   return (
-    <form
-      role="search"
-      className="flex flex-wrap justify-end"
-      onSubmit={(e) => {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        const keyword = formData.get('keyword') as string;
-        router.push(createUrl('/characters', new URLSearchParams({ keyword })));
-      }}
-    >
-      <Input
-        // To reset the `defaultValue` when value from `searchParams` change.
-        key={keyword}
-        id="search"
-        name="keyword"
-        type="search"
-        defaultValue={keyword}
-        aria-label="Search keyword"
-        placeholder="Type to search..."
-        className="flex-1 basis-[30rem]"
-      />
-      <Button type="submit">Search</Button>
-    </form>
+    <search>
+      <form
+        className="flex flex-wrap justify-end"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          const keyword = formData.get('keyword') as string;
+          router.push(
+            createUrl('/characters', new URLSearchParams({ keyword })),
+          );
+        }}
+      >
+        <Input
+          // To reset the `defaultValue` when value from `searchParams` change.
+          key={keyword}
+          id="search"
+          name="keyword"
+          type="search"
+          defaultValue={keyword}
+          aria-label="Search keyword"
+          placeholder="Type to search..."
+          className="flex-1 basis-[30rem]"
+        />
+        <Button type="submit">Search</Button>
+      </form>
+    </search>
   );
 }
